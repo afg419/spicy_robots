7a46961a43b16a2064eccb9102c45c01f840a62a
Aaron Greenspan <afg419@gmail.com>
2015-12-03 15:57:22 -0700

    feat: all moved to sql, all passing

--- test/features/adds_robot_test.rb
+++ test/features/adds_robot_test.rb
@@ -1,36 +1,36 @@
-# require_relative '../test_helper'
-#
-# class AddsRobotTest < FeatureTest
-#
-#   def test_a_user_can_add_a_robot
-#     visit('/')
-#     click_link_or_button('add_new')
-#
-#     assert_equal '/robotregistry/new', current_path
-#
-#     fill_in('robot[name]', with: "X1")
-#     fill_in('robot[city]', with: "X2")
-#     fill_in('robot[state]', with: "X3")
-#     fill_in('robot[operational]', with: "X4")
-#     fill_in('robot[weapons]', with: "X5")
-#     fill_in('robot[branch]', with: "X6")
-#
-#     click_link_or_button('submit')
-#
-#     assert_equal '/robotregistry', current_path
-#
-#     within('#robot_links') do
-#       assert page.has_css?("#X1")
-#     end
-#
-#     click_link_or_button("X1")
-#
-#     within('#robot_data') do
-#       (1..6).each do |num|
-#         assert page.has_content?("X#{num}")
-#       end
-#     end
-#
-#   end
-#
-# end
+require_relative '../test_helper'
+
+class AddsRobotTest < FeatureTest
+
+  def test_a_user_can_add_a_robot
+    visit('/')
+    click_link_or_button('add_new')
+
+    assert_equal '/robotregistry/new', current_path
+
+    fill_in('robot[name]', with: "X1")
+    fill_in('robot[city]', with: "X2")
+    fill_in('robot[state]', with: "X3")
+    fill_in('robot[operational]', with: "X4")
+    fill_in('robot[weapons]', with: "X5")
+    fill_in('robot[branch]', with: "X6")
+
+    click_link_or_button('submit')
+
+    assert_equal '/robotregistry', current_path
+
+    within('#robot_links') do
+      assert page.has_css?("#X1")
+    end
+
+    click_link_or_button("X1")
+
+    within('#robot_data') do
+      (1..6).each do |num|
+        assert page.has_content?("X#{num}")
+      end
+    end
+
+  end
+
+end
--- test/features/deletes_robot_test.rb
+++ test/features/deletes_robot_test.rb
@@ -1,20 +1,20 @@
-# require_relative '../test_helper'
-#
-# class DeletesRobotTest < FeatureTest
-#
-#   def test_a_user_can_delete_a_robot
-#     id = RobotRegistry.create(params.zip(vals(0)).to_h)
-#     visit('/robotregistry')
-#
-#     within('#robot_links') do
-#       assert page.has_css?("#name1")
-#     end
-#
-#     click_link_or_button("delete#{id}")
-#
-#     within('#robot_links') do
-#       refute page.has_css?("#name1")
-#     end
-#   end
-#
-# end
+require_relative '../test_helper'
+
+class DeletesRobotTest < FeatureTest
+
+  def test_a_user_can_delete_a_robot
+    id = RobotRegistry.create(params.zip(vals(0)).to_h)
+    visit('/robotregistry')
+
+    within('#robot_links') do
+      assert page.has_css?("#name1")
+    end
+
+    click_link_or_button("delete#{id}")
+
+    within('#robot_links') do
+      refute page.has_css?("#name1")
+    end
+  end
+
+end
